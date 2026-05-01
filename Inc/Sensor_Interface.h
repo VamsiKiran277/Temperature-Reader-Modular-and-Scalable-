71abb38e61679894e38717dfbbc681cf31969253
Vamsi Kiran <vamsikiran167@gmail.com>
2026-05-01 17:28:20 +0100

    Implemented functions to get microseconds

--- Inc/Sensor_Interface.h
+++ Inc/Sensor_Interface.h
@@ -12,40 +12,61 @@
 #define GPIOD_BASE_ADDRESS    0x40020C00
 #define RCC_BASE_ADDRESS  0x40023800
 #define SYSTICK_BASE_ADDRESS  0xE000E010
+#define TIM2_BASE_ADDRESS  0x40000000
 
 typedef struct {
-	uint32_t RCC_CR; //OFFSET AT 0x00
-	uint32_t RCC_PLLCFGR; //offset at 0x04
-	uint32_t RCC_CFGR;
-	uint32_t RCC_CIR;
-	uint32_t RCC_AHB1RSTR;
-	uint32_t RCC_AHB2RSTR;
-	uint32_t RCC_AHB3RSTR;
-	uint32_t RCC_APB1RSTR;
-	uint32_t RCC_APB2RSTR;
-	uint32_t RCC_AHB1ENR; //offset at 0x30
+	volatile uint32_t RCC_CR; //OFFSET AT 0x00
+	volatile uint32_t RCC_PLLCFGR; //offset at 0x04
+	volatile uint32_t RCC_CFGR;
+	volatile uint32_t RCC_CIR;
+	volatile uint32_t RCC_AHB1RSTR;
+	volatile uint32_t RCC_AHB2RSTR;
+	volatile uint32_t RCC_AHB3RSTR;
+	volatile uint32_t RCC_APB1RSTR;
+	volatile uint32_t RCC_APB2RSTR;
+	volatile uint32_t RCC_AHB1ENR; //offset at 0x30
+	volatile uint32_t RCC_AHB2ENR;
+	volatile uint32_t RCC_AHB3ENR;
+	volatile uint32_t RCC_APB1ENR; //offset at 0x40
 }RCC_t;
 
 typedef struct {
-	uint32_t MODER; //offset at 0x00
-	uint32_t OTYPER; //offset at 0x04
-	uint32_t OSPEEDR;
-	uint32_t PUPDR;
-	uint32_t IDR;
-	uint32_t ODR;
-	uint32_t BSRR;
-	uint32_t LCKR;
-	uint32_t AFRL;
-	uint32_t AFRH; //offset at 0x24
+	volatile uint32_t MODER; //offset at 0x00
+	volatile uint32_t OTYPER; //offset at 0x04
+	volatile uint32_t OSPEEDR;
+	volatile uint32_t PUPDR;
+	volatile uint32_t IDR;
+	volatile uint32_t ODR;
+	volatile uint32_t BSRR;
+	volatile uint32_t LCKR;
+	volatile uint32_t AFRL;
+	volatile uint32_t AFRH; //offset at 0x24
 }GPIO_t;
 
 //check CortexM4 programming manual
 typedef struct {
-	uint32_t CONTROL; //Enables the counter and its interrupt.
-	uint32_t LOAD; //The starting value (24-bit) the timer counts down from.
-	uint32_t VALUE; //The actual live countdown value.
-	uint32_t CALIB;
+	volatile uint32_t CONTROL; //Enables the counter and its interrupt.
+	volatile uint32_t LOAD; //The starting value (24-bit) the timer counts down from.
+	volatile uint32_t VALUE; //The actual live countdown value.
+	volatile uint32_t CALIB;
 }SYSTICK_t;
+
+
+//for TIM2
+typedef struct {
+	volatile uint32_t TIMx_CR1; //offset at 0x00
+	volatile uint32_t TIMx_CR2;
+	volatile uint32_t TIMx_SMCR;
+	volatile uint32_t TIMx_DIER;
+	volatile uint32_t TIMx_SR;
+	volatile uint32_t TIMx_EGR;
+	volatile uint32_t TIMx_CCMR1;
+	volatile uint32_t TIMx_CCMR2;
+	volatile uint32_t TIMx_CCER;
+	volatile uint32_t TIMx_CNT;
+	volatile uint32_t TIMx_PSC;
+	volatile uint32_t TIMx_ARR;
+}TIM2_t;
 
 //Function Definitons
 void BSP_init();
@@ -54,5 +75,6 @@ void BSP_Timer_init(void);
 void SysTick_Handler(void);
 uint32_t BSP_getTick(void);
 void BSP_Delay_ms(uint32_t delay);
+uint32_t BSP_TIM2ENABLE(void);
 
 #endif /* SENSOR_INTERFACE_H_ */
--- Src/Sensor_Interface.c
+++ Src/Sensor_Interface.c
@@ -12,14 +12,18 @@
 #define GPIOA ((GPIO_t*)GPIOA_BASE_ADDRESS)
 #define RCC   ((RCC_t*)RCC_BASE_ADDRESS)
 #define SYSTICK ((SYSTICK_t*)SYSTICK_BASE_ADDRESS)
+#define TIM2   ((TIM2_t*)TIM2_BASE_ADDRESS)
 #define CLK_CYCLE 16000000 // Set to 16MHz to match HSI startup for Renode
 //Bit - (set,clear,toggle)
 #define GPIOA_ENABLE (1<<0)
 #define PA0_CLEAR  (3<<0)
 #define PA0_SET    (1<<0)
 #define CONTROL_ENABLE      (7 << 0)
+#define RCC_APB1_TIM2MEn      (1<<0)
+#define TIM2_Timer_Start    (1<<0)
 //timer
-volatile uint32_t ms_ticks = 0; //hardware timer initialization
+volatile uint32_t ms_ticks = 0; //hardware timer initialization for milli seconds
+
 
 void BSP_Timer_init(void) {
 	SYSTICK->LOAD = ((CLK_CYCLE)/1000) -1; //setting the reload value for 1ms interval
@@ -38,7 +42,7 @@ uint32_t BSP_getTick(void) {
 }
 
 void BSP_Delay_ms(uint32_t delay) {
-	uint32_t start = BSP_getTick();  //get current time
+	volatile uint32_t start = BSP_getTick();  //get current time
 	while((BSP_getTick() - start) < delay);
 }
 void BSP_init() {
@@ -55,3 +59,34 @@ void BSP_DTH11_start(void) {
 	GPIOA->MODER &=~(PA0_CLEAR);
 }
 
+//to get microsecond timer
+uint32_t BSP_TIM2ENABLE(void) {
+	RCC->RCC_APB1ENR |=RCC_APB1_TIM2En; //enable the clock for the TIM2
+	TIM2->TIMx_PSC = 15; //we are using 16MHZ
+	TIM2->TIMx_ARR = 0xFFFF; //seting it to the longest time pssible
+	TIM2->TIMx_CR1 |= TIM2_Timer_Start; //start the time
+}
+
+uint32_t BSP_getMicro(void) {
+	return TIM2->TIMx_CNT;
+}
+
+uint32_t BSP_DTH11_Response(void) {
+	uint32_t timeout;
+	//wait for pin to go low
+	timeout = BSP_getMicro();
+	while(GPIOA->IDR & (PA0_SET)) {
+		if((BSP_getMicros() - timeout) > 100) return 0; //Sensor Didnt pull low
+	}
+	//wait for pin to go high, DHT11 releasing after 80us
+	timeout = BSP_getMicro();
+	while (!(GPIOA->IDR & (PA0_SET))) { //While PA0 is still LOW
+	        if ((BSP_getMicros() - timeout) > 100) return 0; //Stayed low too long
+	}
+	//wait for pin to go low again
+	timeout = BSP_getMicro();
+	while (GPIOA->IDR & (PA0_SET)) { // While PA0 is still HIGH
+	        if ((BSP_getMicros() - timeout) > 100) return 0; // Error: Stayed high too long
+	}
+	return 1; //handshake complete
+}
